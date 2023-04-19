78dc1dabe48d24f7093abc5814312defa4bf872c
Faith Lovell <faithlxvell@gmail.com>
2023-04-18 22:58:30 -0400

    appearance, layout of where to put components

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">The Film Feed</header>
+            <p>Select Role: *selection here*</p>
+            <p>Movies List: *movies list (super list) here*</p>
+            <p>Your List: *user list here*</p>
             <p>
                 Katie Oates, Diya Shah, John Henry Cooper, Faith Lovell, Joy
                 Mwaria
-            </p>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
             </p>
         </div>
     );

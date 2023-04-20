d3beb0ac02ef84d6b726f33da2bdd0f900c6ac16
Joy <jkmwaria@udel.edu>
2023-04-20 16:13:38 -0400

    more linter issues

--- src/MovieMaster.tsx
+++ src/MovieMaster.tsx
@@ -1,17 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useState } from "react";
 //import { Form } from "react-bootstrap";
 
 export function MovieMaster(): JSX.Element {
-    const [movie, setMovie] = useState<string>("Your Name");
-
-    // function updateMovie() {
-    //     setMovie(movie);
-    // }
-
-    // This is the View
     return (
         <div>
-            <h3>Movie Master List</h3>
+            <h3> Movie Master List</h3>
         </div>
     );
 }

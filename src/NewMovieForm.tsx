696fcdcc6ff636c77f4b7729637814e9836d19a9
Faith Lovell <faithlxvell@gmail.com>
2023-05-08 03:19:49 -0400

    movie input form changes

---
+++ src/NewMovieForm.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useState } from "react";
+import "./MovieForm.css";
+
+/*interface Review {
+    name: string;
+    content: string;
+}*/
+
+export function AddMovie() {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    const [actors, setActors] = useState<string[]>([]);
+    const [rating, setRating] = useState(0);
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
+    const handleDescriptionChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        setDescription(event.target.value);
+    };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setImage(event.target.value);
+    };
+
+    const handleRatingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRating(parseInt(event.target.value));
+    };
+
+    const handleActorsChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        const inputText = event.target.value;
+        const newActors = inputText.split(",").map((actor) => actor.trim());
+        setActors(newActors);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <h1>Add a Movie</h1>
+                    <label htmlFor="name">Movie Name: </label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description">Description: </label>
+                    <textarea
+                        id="description"
+                        value={description}
+                        onChange={handleDescriptionChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="image">Image: </label>
+                    <input
+                        type="text"
+                        id="image"
+                        value={image}
+                        onChange={handleImageChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="actors">Actor List: </label>
+                    <textarea
+                        id="actors"
+                        value={actors}
+                        onChange={handleActorsChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="rating">Number Rating: </label>
+                    <input
+                        type="number"
+                        id="rating"
+                        value={rating}
+                        min="0"
+                        max="10"
+                        onChange={handleRatingChange}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}

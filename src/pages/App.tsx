ff014d2c622781617e1ae7ef81ecdff7bc66153e
Joy <jkmwaria@udel.edu>
2023-05-09 15:39:09 -0400

    improved visuals

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../App.css";
 import { RoleSelect } from "../MenuBar";
-import logo from "../assets/other-ff-logo.png";
+import logo from "../assets/ff-logo.png";
 import { DragLists } from "../DragList";
 import "../styles.css";
 import "../Scroller.css";
@@ -305,7 +305,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 <div className="container">
                     <img
-                        style={{ width: 500, height: 131 }}
+                        style={{ width: 280, height: 241 }}
                         src={logo}
                         alt="logo"
                     />

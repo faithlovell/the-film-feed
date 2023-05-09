2c14776455d22c0b0905107d985026a11f51cfff
John Henry Cooper <122650638+jhcooper@users.noreply.github.com>
2023-05-08 20:11:31 -0400

    Working Movie Filtering

--- src/AllMoviesList.tsx
+++ src/AllMoviesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AllMoviesList.css"; // import CSS file
 import { Movie, MovieItem } from "./MovieMaster";
 
@@ -9,13 +9,52 @@ interface AllMoviesListProps {
 }
 
 export function AllMoviesList({ movies, onSave, role }: AllMoviesListProps) {
+    const [filter, setFilter] = useState("");
+
+    const handleFilterChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setFilter(event.target.value);
+    };
+
+    const filteredMovies = movies.filter((movie) => {
+        if (filter === "") {
+            return true;
+        } else if (filter === "inTheaters") {
+            return movie.inTheaters;
+        } else if (filter === "ratingG") {
+            return movie.rating === "G";
+        } else if (filter === "ratingPG") {
+            return movie.rating === "PG";
+        } else if (filter === "ratingPG13") {
+            return movie.rating === "PG-13";
+        } else if (filter === "ratingR") {
+            return movie.rating === "R";
+        }
+        return false;
+    });
+
     return (
         <>
             {role === "Movie Mentor" || role === "Movie Master" ? (
                 <div className="all-movies-list">
                     <h2 className="title">All Movies</h2>
+                    <div className="filters">
+                        <select
+                            value={filter}
+                            onChange={handleFilterChange}
+                            className="filter-dropdown"
+                        >
+                            <option value="">All</option>
+                            <option value="inTheaters">In Theaters</option>
+                            <option value="ratingG">G</option>
+                            <option value="ratingPG">PG</option>
+                            <option value="ratingPG13">PG-13</option>
+                            <option value="ratingR">R</option>
+                        </select>
+                    </div>
                     <div className="movie-images">
-                        {movies.map((movie) => (
+                        {filteredMovies.map((movie) => (
                             <MovieItem
                                 key={movie.id}
                                 movie={movie}

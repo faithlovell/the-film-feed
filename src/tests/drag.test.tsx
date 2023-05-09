93c92d3278be6d958e2e7bb0223c5a45830016ed
Katie <koates@udel.edu>
2023-05-09 13:40:37 -0400

    more tests

---
+++ src/tests/drag.test.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { DragLists } from "../DragList";
+
+describe("DragLists", () => {
+    const mockMovies = [
+        {
+            id: 1,
+            title: "Movie 1",
+            cast: ["Actor 1", "Actor 2"],
+            rating: "PG",
+            audienceRating: 0,
+            inTheaters: true,
+            image: "movie1.jpg",
+            description: "Movie 1 description"
+        }
+        // Add more movies as needed
+    ];
+
+    test("renders DragLists component with Super List", () => {
+        const { getByText } = render(<DragLists role="Movie Master" />);
+
+        const superListLabel = getByText("Super List");
+        expect(superListLabel).toBeInTheDocument();
+    });
+
+    test("renders DragLists component with Admin List", () => {
+        const { getByText } = render(<DragLists role="Movie Mentor" />);
+
+        const adminListLabel = getByText("Admin List");
+        expect(adminListLabel).toBeInTheDocument();
+    });
+
+    test("renders DragLists component with User List", () => {
+        const { getByText } = render(<DragLists role="Movie Member" />);
+
+        const userListLabel = getByText("User List");
+        expect(userListLabel).toBeInTheDocument();
+    });
+
+    test("calls handleOnDropSuper when a movie is dropped on Super List", () => {
+        const handleOnDropSuperMock = jest.fn();
+        const { getByTestId } = render(<DragLists role="Movie Master" />);
+
+        const superList = getByTestId("super-list");
+        fireEvent.drop(superList);
+
+        expect(handleOnDropSuperMock).toHaveBeenCalledTimes(1);
+    });
+
+    test("calls handleOnDropAdmin when a movie is dropped on Admin List", () => {
+        const handleOnDropAdminMock = jest.fn();
+        const { getByTestId } = render(<DragLists role="Movie Mentor" />);
+
+        const adminList = getByTestId("admin-list");
+        fireEvent.drop(adminList);
+
+        expect(handleOnDropAdminMock).toHaveBeenCalledTimes(1);
+    });
+
+    test("calls handleOnDropUser when a movie is dropped on User List", () => {
+        const handleOnDropUserMock = jest.fn();
+        const { getByTestId } = render(<DragLists role="Movie Member" />);
+
+        const userList = getByTestId("user-list");
+        fireEvent.drop(userList);
+
+        expect(handleOnDropUserMock).toHaveBeenCalledTimes(1);
+    });
+});
---
+++ src/tests/moviesList.test.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { AllMoviesList } from "../AllMoviesList";
+
+describe("AllMoviesList", () => {
+    const movies = [
+        {
+            id: 1,
+            title: "Movie 1",
+            cast: ["Actor 1", "Actor 2"],
+            rating: "PG",
+            audienceRating: 0,
+            inTheaters: true,
+            image: "movie1.jpg",
+            description: "Movie 1 description"
+        }
+        // Add more movies as needed
+    ];
+
+    const onSaveMock = jest.fn();
+    const onDeleteMock = jest.fn();
+
+    test("renders AllMoviesList component with movies", () => {
+        const { getByText } = render(
+            <AllMoviesList
+                movies={movies}
+                onSave={onSaveMock}
+                onDelete={onDeleteMock}
+                role="Movie Mentor"
+                draggable={false}
+                onDragStart={jest.fn()}
+            />
+        );
+
+        // Check if the movies are rendered
+        movies.forEach((movie) => {
+            const movieTitleElement = getByText(movie.title);
+            expect(movieTitleElement).toBeInTheDocument();
+        });
+    });
+
+    test("calls onSave function when a movie is saved", () => {
+        const { getAllByText } = render(
+            <AllMoviesList
+                movies={movies}
+                onSave={onSaveMock}
+                onDelete={onDeleteMock}
+                role="Movie Mentor"
+                draggable={false}
+                onDragStart={jest.fn()}
+            />
+        );
+
+        // Get the save buttons and click one of them
+        const saveButton = getAllByText("Save")[0];
+        fireEvent.click(saveButton);
+
+        // Check if onSave function is called
+        expect(onSaveMock).toHaveBeenCalledTimes(1);
+    });
+
+    test("calls onDelete function when a movie is deleted", () => {
+        const { getAllByText } = render(
+            <AllMoviesList
+                movies={movies}
+                onSave={onSaveMock}
+                onDelete={onDeleteMock}
+                role="Movie Mentor"
+                draggable={false}
+                onDragStart={jest.fn()}
+            />
+        );
+
+        // Get the delete buttons and click one of them
+        const deleteButton = getAllByText("Delete")[0];
+        fireEvent.click(deleteButton);
+
+        // Check if onDelete function is called
+        expect(onDeleteMock).toHaveBeenCalledTimes(1);
+    });
+
+    test("updates the filter when the dropdown value changes", () => {
+        const { getByLabelText } = render(
+            <AllMoviesList
+                movies={movies}
+                onSave={onSaveMock}
+                onDelete={onDeleteMock}
+                role="Movie Mentor"
+                draggable={false}
+                onDragStart={jest.fn()}
+            />
+        );
+
+        // Get the filter dropdown and change its value
+        const filterDropdown = getByLabelText("Filter");
+        fireEvent.change(filterDropdown, { target: { value: "ratingPG" } });
+
+        // Check if the filter value is updated
+        expect(filterDropdown).toBe("ratingPG");
+    });
+});

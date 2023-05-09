ae696ffe251f602c132388290fdc206025950d6d
Katie <koates@udel.edu>
2023-05-09 13:27:56 -0400

    test files

---
+++ src/tests/menubar.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { RoleSelect } from "../MenuBar";
+
+describe("RoleSelect", () => {
+    const options = ["Option 1", "Option 2", "Option 3"];
+
+    test("renders RoleSelect component with options", () => {
+        const { getByText } = render(
+            <RoleSelect role="Option 1" setRole={jest.fn()} options={options} />
+        );
+
+        // Check if the component renders correctly
+        const titleElement = getByText("Role Selection");
+        expect(titleElement).toBeInTheDocument();
+
+        // Check if the options are rendered
+        options.forEach((option) => {
+            const optionElement = getByText(option);
+            expect(optionElement).toBeInTheDocument();
+        });
+    });
+
+    test("calls setRole function on option selection", () => {
+        const setRole = jest.fn();
+
+        const { getByLabelText } = render(
+            <RoleSelect role="Option 1" setRole={setRole} options={options} />
+        );
+
+        // Simulate selecting an option
+        const selectElement = getByLabelText("Role Selection");
+        fireEvent.change(selectElement, { target: { value: "Option 2" } });
+
+        // Check if setRole function is called with the selected value
+        expect(setRole).toHaveBeenCalledWith("Option 2");
+    });
+});
---
+++ src/tests/slider.test.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import SliderParent from "../Scroller";
+
+describe("SliderParent", () => {
+    const movies = [
+        { title: "Movie 1", image: "image1.jpg", rating: 4.5 },
+        { title: "Movie 2", image: "image2.jpg", rating: 3.8 },
+        { title: "Movie 3", image: "image3.jpg", rating: 4.2 }
+    ];
+
+    test("renders SliderParent component with movies", () => {
+        const { getByText, getAllByAltText } = render(
+            <SliderParent movies={movies} />
+        );
+
+        // Check if the movies are rendered
+        movies.forEach((movie) => {
+            const movieTitleElement = getByText(movie.title);
+            expect(movieTitleElement).toBeInTheDocument();
+
+            const movieImageElement = getAllByAltText(movie.title)[0];
+            expect(movieImageElement).toBeInTheDocument();
+        });
+    });
+
+    test("scrolls the slider on next button click", () => {
+        const { getByText, container } = render(
+            <SliderParent movies={movies} />
+        );
+
+        // Get the next button and click it
+        const nextButton = getByText(">");
+        fireEvent.click(nextButton);
+
+        // Check if the slider has scrolled
+        const sliderWrapper = container.querySelector(".slider-wrapper");
+        expect(sliderWrapper?.scrollLeft).toBeGreaterThan(0);
+    });
+
+    test("scrolls the slider on previous button click", () => {
+        const { getByText, container } = render(
+            <SliderParent movies={movies} />
+        );
+
+        // Get the previous button and click it
+        const prevButton = getByText("<");
+        fireEvent.click(prevButton);
+
+        // Check if the slider has scrolled
+        const sliderWrapper = container.querySelector(".slider-wrapper");
+        expect(sliderWrapper?.scrollLeft).toBeLessThan(0);
+    });
+});

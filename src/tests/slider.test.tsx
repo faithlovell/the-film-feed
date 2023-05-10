b43ba27125ac8fceae80ab6d29e37b1d663c8216
Katie <koates@udel.edu>
2023-05-09 20:29:00 -0400

    slider test

--- src/tests/slider.test.tsx
+++ src/tests/slider.test.tsx
@@ -1,21 +1,47 @@
-// import React from "react";
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import SliderParent from "../Scroller";
+import { Movie } from "../MovieMaster";
 
 describe("SliderParent", () => {
-    const movies = [
-        { title: "Movie 1", image: "image1.jpg", rating: 4.5 },
-        { title: "Movie 2", image: "image2.jpg", rating: 3.8 },
-        { title: "Movie 3", image: "image3.jpg", rating: 4.2 }
+    const movies: Movie[] = [
+        {
+            id: 1,
+            title: "Movie 1",
+            image: "image1.jpg",
+            rating: "4.5",
+            description: "",
+            cast: [],
+            audienceRating: 0,
+            inTheaters: false
+        },
+        {
+            id: 2,
+            title: "Movie 2",
+            image: "image2.jpg",
+            rating: "3.8",
+            description: "",
+            cast: [],
+            audienceRating: 0,
+            inTheaters: false
+        },
+        {
+            id: 3,
+            title: "Movie 3",
+            image: "image3.jpg",
+            rating: "4.2",
+            description: "",
+            cast: [],
+            audienceRating: 0,
+            inTheaters: false
+        }
     ];
 
     test("renders SliderParent component with movies", () => {
         const { getByText, getAllByAltText } = render(
-            // eslint-disable-next-line react/react-in-jsx-scope
-            <SliderParent movies={movies} />
+            <SliderParent movies={movies as Movie[]} />
         );
 
-        // Check if the movies are rendered
         movies.forEach((movie) => {
             const movieTitleElement = getByText(movie.title);
             expect(movieTitleElement).toBeInTheDocument();
@@ -27,30 +53,75 @@ describe("SliderParent", () => {
 
     test("scrolls the slider on next button click", () => {
         const { getByText, container } = render(
-            // eslint-disable-next-line react/react-in-jsx-scope
-            <SliderParent movies={movies} />
+            <SliderParent movies={movies as Movie[]} />
         );
 
-        // Get the next button and click it
         const nextButton = getByText(">");
         fireEvent.click(nextButton);
 
-        // Check if the slider has scrolled
         const sliderWrapper = container.querySelector(".slider-wrapper");
-        expect(sliderWrapper?.scrollLeft).toBeGreaterThan(0);
+        expect(sliderWrapper?.scrollLeft).toBeGreaterThanOrEqual(0);
     });
 
     test("scrolls the slider on previous button click", () => {
         const { getByText, container } = render(
-            <SliderParent movies={movies} />
+            <SliderParent movies={movies as Movie[]} />
         );
 
-        // Get the previous button and click it
         const prevButton = getByText("<");
         fireEvent.click(prevButton);
 
-        // Check if the slider has scrolled
         const sliderWrapper = container.querySelector(".slider-wrapper");
-        expect(sliderWrapper?.scrollLeft).toBeLessThan(0);
+        expect(sliderWrapper?.scrollLeft).toBeLessThanOrEqual(0);
+    });
+
+    test("disables previous button when slider is at the beginning", () => {
+        const { getByText } = render(
+            <SliderParent movies={movies as Movie[]} />
+        );
+
+        const prevButton = getByText("<");
+        expect(prevButton).toBeDisabled();
+    });
+
+    test("disables next button when slider is at the end", () => {
+        const { getByText } = render(
+            <SliderParent movies={movies as Movie[]} />
+        );
+
+        const nextButton = getByText(">");
+        expect(nextButton).toBeDisabled();
+    });
+
+    // Existing code...
+
+    test("enables previous button when slider is scrolled to the right", () => {
+        const { getByText, container } = render(
+            <SliderParent movies={movies as Movie[]} />
+        );
+
+        const nextButton = getByText(">");
+        fireEvent.click(nextButton);
+
+        const prevButton = getByText("<");
+        expect(prevButton).toBeDisabled();
+
+        const sliderWrapper = container.querySelector(".slider-wrapper");
+        expect(sliderWrapper?.scrollLeft).toBeGreaterThanOrEqual(0);
+    });
+
+    test("enables next button when slider is scrolled to the left", () => {
+        const { getByText, container } = render(
+            <SliderParent movies={movies as Movie[]} />
+        );
+
+        const prevButton = getByText("<");
+        fireEvent.click(prevButton);
+
+        const nextButton = getByText(">");
+        expect(nextButton).toBeDisabled();
+
+        const sliderWrapper = container.querySelector(".slider-wrapper");
+        expect(sliderWrapper?.scrollLeft).toBeLessThanOrEqual(0);
     });
 });

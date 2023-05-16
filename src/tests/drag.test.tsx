6c5f3d6b3b84b4811ce92cd2a28c5d66c26ee57d
Katie <koates@udel.edu>
2023-05-15 22:27:45 -0400

    test changes

---
+++ src/tests/drag.test.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { DragLists } from "./DragLists";
+
+describe("DragLists", () => {
+    it("should add a new user when enter key is pressed", () => {
+        const setOptions = jest.fn();
+        const setMembers = jest.fn();
+        const updateMovieCount = jest.fn();
+
+        render(
+            <DragLists
+                role="Movie Master"
+                options={["User1", "User2"]}
+                setOptions={setOptions}
+                adminMovies={[]}
+                setAdminMovies={jest.fn()}
+                userMovies={[]}
+                setUserMovies={jest.fn()}
+                onDelete={jest.fn()}
+                handleAdminOnSave={jest.fn()}
+                handleUserOnSave={jest.fn()}
+                movieCounts={{}}
+                setMovieCounts={jest.fn()}
+            />
+        );
+
+        const newUserInput = screen.getByLabelText("New User:");
+        userEvent.type(newUserInput, "NewUser{enter}");
+
+        expect(setOptions).toHaveBeenCalledWith(["User1", "User2", "NewUser"]);
+        expect(setMembers).toHaveBeenCalledWith(["User1", "User2", "NewUser"]);
+        expect(updateMovieCount).toHaveBeenCalledWith("NewUser", 0);
+    });
+
+    it("should display admin movies when role is Movie Mentor", () => {
+        const adminMovies = [
+            { id: 1, title: "Movie 1" },
+            { id: 2, title: "Movie 2" }
+        ];
+
+        render(
+            <DragLists
+                role="Movie Mentor"
+                options={[]}
+                setOptions={jest.fn()}
+                adminMovies={adminMovies}
+                setAdminMovies={jest.fn()}
+                userMovies={[]}
+                setUserMovies={jest.fn()}
+                onDelete={jest.fn()}
+                handleAdminOnSave={jest.fn()}
+                handleUserOnSave={jest.fn()}
+                movieCounts={{}}
+                setMovieCounts={jest.fn()}
+            />
+        );
+
+        expect(screen.getByText("Movie Mentor List")).toBeInTheDocument();
+        expect(screen.getByText("Movie 1")).toBeInTheDocument();
+        expect(screen.getByText("Movie 2")).toBeInTheDocument();
+    });
+
+    it("should display user movies when role is not Movie Master or Movie Mentor", () => {
+        const userMovies = [
+            { id: 1, title: "Movie 1" },
+            { id: 2, title: "Movie 2" }
+        ];
+
+        render(
+            <DragLists
+                role="User Editor"
+                options={[]}
+                setOptions={jest.fn()}
+                adminMovies={[]}
+                setAdminMovies={jest.fn()}
+                userMovies={userMovies}
+                setUserMovies={jest.fn()}
+                onDelete={jest.fn()}
+                handleAdminOnSave={jest.fn()}
+                handleUserOnSave={jest.fn()}
+                movieCounts={{}}
+                setMovieCounts={jest.fn()}
+            />
+        );
+
+        expect(screen.getByText("User Editor List")).toBeInTheDocument();
+        expect(screen.getByText("Movie 1")).toBeInTheDocument();
+        expect(screen.getByText("Movie 2")).toBeInTheDocument();
+    });
+});

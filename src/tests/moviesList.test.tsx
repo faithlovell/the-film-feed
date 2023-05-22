0600b9bcdca7c58f86146c5d7f60e398646ab1de
Katie <koates@udel.edu>
2023-05-21 21:32:36 -0400

    more test changes

--- src/tests/moviesList.test.tsx
+++ src/tests/moviesList.test.tsx
@@ -1,128 +1,105 @@
-// /* eslint-disable react/react-in-jsx-scope */
-// // import React from "react";
-// import { render, fireEvent } from "@testing-library/react";
-// import { AllMoviesList } from "../AllMoviesList";
-//
-// describe("AllMoviesList", () => {
-//     const movies = [
-//         {
-//             id: 1,
-//             title: "Movie 1",
-//             cast: ["Actor 1", "Actor 2"],
-//             rating: "PG",
-//             audienceRating: 0,
-//             inTheaters: true,
-//             image: "movie1.jpg",
-//             description: "Movie 1 description"
-//         }
-//         // Add more movies as needed
-//     ];
-//
-//     const onSaveMock = jest.fn();
-//     const onDeleteMock = jest.fn();
-//
-//     test("renders AllMoviesList component with movies", () => {
-//         const { queryAllByText } = render(
-//             <AllMoviesList
-//                 movies={movies}
-//                 onSave={onSaveMock}
-//                 onDelete={onDeleteMock}
-//                 role="Movie Mentor"
-//                 draggable={false}
-//                 onDragStart={jest.fn()}
-//                 user={role}
-//             />
-//         );
-//
-//         movies.forEach((movie) => {
-//             const movieTitleElements = queryAllByText(movie.title);
-//             expect(movieTitleElements.length).toBeGreaterThan(0);
-//         });
-//     });
-//
-//     test("calls onSave function when a movie is saved", () => {
-//         const { queryByText } = render(
-//             <AllMoviesList
-//                 movies={movies}
-//                 onSave={onSaveMock}
-//                 onDelete={onDeleteMock}
-//                 role="Movie Mentor"
-//                 draggable={false}
-//                 onDragStart={jest.fn()}
-//             />
-//         );
-//
-//         const saveButton = queryByText("Save");
-//         if (saveButton) {
-//             fireEvent.click(saveButton);
-//             expect(onSaveMock).toHaveBeenCalledTimes(1);
-//         }
-//     });
-//
-//     test("calls onDelete function when a movie is deleted", () => {
-//         const { queryByText } = render(
-//             <AllMoviesList
-//                 movies={movies}
-//                 onSave={onSaveMock}
-//                 onDelete={onDeleteMock}
-//                 role="Movie Mentor"
-//                 draggable={false}
-//                 onDragStart={jest.fn()}
-//             />
-//         );
-//
-//         const deleteButton = queryByText("Delete");
-//         if (deleteButton) {
-//             fireEvent.click(deleteButton);
-//             expect(onDeleteMock).toHaveBeenCalledTimes(1);
-//         }
-//     });
-//
-//     test("updates the filter when the dropdown value changes", () => {
-//         const { getByRole, queryAllByText } = render(
-//             <AllMoviesList
-//                 movies={movies}
-//                 onSave={onSaveMock}
-//                 onDelete={onDeleteMock}
-//                 role="Movie Mentor"
-//                 draggable={false}
-//                 onDragStart={jest.fn()}
-//             />
-//         );
-//
-//         const filterDropdown = getByRole("combobox");
-//         fireEvent.change(filterDropdown, { target: { value: "ratingPG" } });
-//
-//         const filteredMovies = movies.filter((movie) => movie.rating === "PG");
-//         filteredMovies.forEach((movie) => {
-//             const movieTitleElements = queryAllByText(
-//                 new RegExp(movie.title, "i")
-//             );
-//             expect(movieTitleElements.length).toBeGreaterThan(0);
-//         });
-//     });
-//     test("updates the filter when the dropdown value changes", () => {
-//         const { getByRole, queryAllByText } = render(
-//             <AllMoviesList
-//                 movies={movies}
-//                 onSave={onSaveMock}
-//                 onDelete={onDeleteMock}
-//                 role="Movie Mentor"
-//                 draggable={false}
-//                 onDragStart={jest.fn()}
-//             />
-//         );
-//
-//         const filterDropdown = getByRole("combobox");
-//         fireEvent.change(filterDropdown, { target: { value: "ratingPG" } });
-//
-//         const filteredMovies = movies.filter((movie) => movie.rating === "PG");
-//         filteredMovies.forEach((movie) => {
-//             const movieTitleElements = queryAllByText(
-//                 new RegExp(movie.title, "i")
-//             );
-//             expect(movieTitleElements.length).toBeGreaterThan(0);
-//         });
-//     });
-// });
-export {};
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { AllMoviesList } from "../AllMoviesList";
+import { Movie } from "../MovieMaster";
+
+// Sample test data
+const movies = [
+    {
+        id: 1,
+        title: "Movie 1",
+        rating: "PG",
+        inTheaters: true
+    },
+    {
+        id: 2,
+        title: "Movie 2",
+        rating: "R",
+        inTheaters: false
+    }
+];
+
+// Mocked functions
+const onSaveMock = jest.fn();
+const onDeleteMock = jest.fn();
+const onDragStartMock = jest.fn();
+const countMovieOccurrencesMock = jest.fn();
+
+describe("AllMoviesList", () => {
+    beforeEach(() => {
+        render(
+            <AllMoviesList
+                movies={[]}
+                onSave={onSaveMock}
+                onDelete={onDeleteMock}
+                role="user"
+                user="John"
+                countMovieOccurrences={countMovieOccurrencesMock}
+                draggable={false}
+                onDragStart={function (
+                    e: React.DragEvent<HTMLDivElement>,
+                    movie: Movie
+                ): void {
+                    throw new Error("Function not implemented.");
+                }}
+            />
+        );
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("renders the movie list correctly", () => {
+        // Check if the movie list is rendered
+        const movieListElement = screen.getByTestId("movie-list");
+        expect(movieListElement).toBeInTheDocument();
+
+        // Check if the correct number of movie items is rendered
+        const movieItems = screen.getAllByTestId("movie-item");
+        expect(movieItems.length).toBe(movies.length);
+
+        // Check if the movie titles are rendered correctly
+        movies.forEach((movie) => {
+            const movieTitleElement = screen.getByText(movie.title);
+            expect(movieTitleElement).toBeInTheDocument();
+        });
+    });
+
+    it("calls onSave function when a movie is saved", () => {
+        const saveButton = screen.getAllByTestId("save-button")[0];
+        fireEvent.click(saveButton);
+
+        expect(onSaveMock).toHaveBeenCalledTimes(1);
+        expect(onSaveMock).toHaveBeenCalledWith(movies[0]);
+    });
+
+    it("calls onDelete function when a movie is deleted", () => {
+        const deleteButton = screen.getAllByTestId("delete-button")[0];
+        fireEvent.click(deleteButton);
+
+        expect(onDeleteMock).toHaveBeenCalledTimes(1);
+        expect(onDeleteMock).toHaveBeenCalledWith(movies[0]);
+    });
+
+    it("calls onDragStart function when a movie is dragged", () => {
+        const movieItem = screen.getAllByTestId("movie-item")[0];
+        fireEvent.dragStart(movieItem);
+
+        expect(onDragStartMock).toHaveBeenCalledTimes(1);
+        expect(onDragStartMock).toHaveBeenCalledWith(
+            expect.any(Object),
+            movies[0]
+        );
+    });
+
+    it("filters movies correctly based on the selected filter", () => {
+        const filterDropdown = screen.getByRole("combobox");
+        fireEvent.change(filterDropdown, { target: { value: "ratingPG" } });
+
+        const movieItems = screen.getAllByTestId("movie-item");
+
+        expect(movieItems.length).toBe(1);
+        expect(screen.getByText("Movie 1")).toBeInTheDocument();
+    });
+});
